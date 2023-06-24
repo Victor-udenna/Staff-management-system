459865104a342debf8b80b651421ccdeb310dec7
Victor-udenna <victorudenna@gmail.com>
2023-06-24 15:44:55 +0100

    Added all files

---
+++ src/Components/Config/firebase-config.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMjIVHQswxON4by9RkizQOlc34Aidennc",
+  authDomain: "staffee-b211f.firebaseapp.com",
+  projectId: "staffee-b211f",
+  storageBucket: "staffee-b211f.appspot.com",
+  messagingSenderId: "13727640612",
+  appId: "1:13727640612:web:be7eb01b03e613659c7cb1",
+  measurementId: "G-BMZL4XZ5GC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
