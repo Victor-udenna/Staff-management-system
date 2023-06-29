25d82f5c660466c46840ac2d565681ff7225cf04
Victor-udenna <victorudenna@gmail.com>
2023-06-29 16:01:41 +0100

    Added login and signup functionality

--- src/Components/Pages/Login/Login.tsx
+++ src/Components/Pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Login.scss";
 import Input from "../../atoms/Input/Input";
 import Image from "../../atoms/Image/Image";
@@ -6,38 +7,74 @@ import { Button } from "../../atoms/Button/Button";
 import HeaderText from "../../atoms/HeaderText/HeaderText";
 import { Link } from "react-router-dom";
 import GoogleButton from "../../atoms/GoogleButton/GoogleButton";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../Config/firebase-config";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  console.log(email, password)
 
+  const loginFunction = (e: React.FormEvent) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).then((userdetails) =>{
+      const user = userdetails;
+      navigate("/dashboard")
+      console.log(user);
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage)
+  });
+  };
 
   return (
-<main  className="signin__container">
-  <div className="signin__img__container">
-<Image  className="signin__img" image={bgImg} alt="bg-image"/>
-  </div>
+    <main className="signin__container">
+      <div className="signin__img__container">
+        <Image className="signin__img" image={bgImg} alt="bg-image" />
+      </div>
 
-  <div className="sign__in__form__container">
- <HeaderText  classname="log_in_header" value="Hello Again!"/>   
- <div className="header_text"><p>Welcome back, Enter your information to log into your</p>
- <p> account.</p></div>
-<form className="sign__in__form">
-<div className="sign_in_input_container">
-  <p className="label">Email Adress</p>
-  <Input type="email"/>
-  </div>
+      <div className="sign__in__form__container">
+        <HeaderText classname="log_in_header" value="Hello Again!" />
+        <div className="header_text">
+          <p>Welcome back, Enter your information to log into your</p>
+          <p> account.</p>
+        </div>
+        <form className="sign__in__form" onSubmit={loginFunction}>
+          <div className="sign_in_input_container">
+            <p className="label">Email Adress</p>
+            <Input
+              type="email"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
 
-  <div className="sign_in_input_container">
-  <p className="label">Password</p>
-  <Input type="password"/>
-  </div>
-  <Button classname="sign_in_button" value="Log in"/>
-  <div className="log__in__link"><span>Don't have an account ?</span> <Link to="/signup"><span>Sign up here</span></Link></div>
-</form>
-<GoogleButton value="Continue with Google" onclick={()=>{}}/>
-  </div>
-</main>
-  )
-}
+          <div className="sign_in_input_container">
+            <p className="label">Password</p>
+            <Input
+              type="password"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <Button classname="sign_in_button" value="Log in" />
+          <div className="log__in__link">
+            <span>Don't have an account ?</span>{" "}
+            <Link to="/signup">
+              <span>Sign up here</span>
+            </Link>
+          </div>
+        </form>
+        <GoogleButton value="Continue with Google" onclick={() => {}} />
+      </div>
+    </main>
+  );
+};
 
-export default Login
+export default Login;
--- src/Components/Pages/Signup/Signup.tsx
+++ src/Components/Pages/Signup/Signup.tsx
@@ -7,13 +7,35 @@ import { Button } from "../../atoms/Button/Button";
 import bgImg from "../../../assets/undraw_account_re_o7id.svg";
 import { Link } from "react-router-dom";
 import GoogleButton from "../../atoms/GoogleButton/GoogleButton";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../Config/firebase-config"
+import { updateProfile } from "firebase/auth";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Signup = () => {
 
-const [userName, setUserName] = useState("");
+const [username, setUserName] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
+
+ const navigate = useNavigate();
+
+const createUser = async (e: React.FormEvent)=>{
+e.preventDefault()
+if (username.length == 0) {
+  alert("name cannot be empty");
+} else {
+  const { user } = await createUserWithEmailAndPassword(auth, email, password)
+  console.log(`User ${user.uid} created`)
+  await updateProfile(user, {
+    displayName: username
+  });
+  console.log("User profile updated")
+  navigate("/");
+}
+}
+
+console.log(auth.currentUser);
 
   return (
     <main  className="signup__container">
@@ -24,18 +46,27 @@ const [password, setPassword] = useState("");
  <HeaderText  classname="sign_up_header" value="Hello!"/>   
  <div className="header_text"><p>Fill in the necesarry details below to create your</p>
  <p> account.</p></div>
-<form className="sign__up__form">
+<form className="sign__up__form" onSubmit={createUser}>
 <div className="sign_up_input_container">
   <p className="label">User name</p>
-  <Input type="text" required={true}/>
+  <Input type="text" required={true}
+  onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setUserName(e.target.value)
+  }}/>
   </div>
 <div className="sign_up_input_container">
   <p className="label">Email Adress</p>
-  <Input type="email" required={true}/>
+  <Input type="email" required={true}
+  onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setEmail(e.target.value)
+  }}/>
   </div>
   <div className="sign_up_input_container">
   <p className="label">Password</p>
-  <Input type="password" required={true}/>
+  <Input type="password" required={true}
+  onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setPassword(e.target.value)
+  }}/>
   </div>
   <Button classname="sign_up_button" value="Create Account"/>
   <div className="log__in__link"><span>Already have an account yet ?</span> <Link to="/"><span>Log in here</span></Link></div>
