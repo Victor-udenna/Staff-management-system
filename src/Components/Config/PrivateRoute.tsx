1f1186326a857e841d30f7b424ab561f4e52ae66
Victor-udenna <victorudenna@gmail.com>
2023-09-11 10:59:59 +0100

    Edited  login logic

--- src/Components/Config/PrivateRoute.tsx
+++ src/Components/Config/PrivateRoute.tsx
@@ -1,13 +1,17 @@
-import { memo } from "react";
-import { Navigate } from "react-router-dom";
+import { memo} from "react";
+import {Navigate} from 'react-router-dom';
 
 type ChildrenProps = {
   children: JSX.Element;
 };
 
-const PrivateRoute = ({ children }: ChildrenProps) => {
-  let authState = sessionStorage.getItem("authState");
 
-  return authState !== "" ? children : <Navigate to="/" />;
-};
-export default memo(PrivateRoute);
+const PrivateRoute = ({children}: ChildrenProps) => {
+
+
+
+let authState = sessionStorage.getItem("authState")
+
+return authState !== null  ? children : <Navigate to="/"/> 
+}
+export default memo(PrivateRoute)
