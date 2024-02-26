cee2f4fb885d7ed7063fca5b4f45601a1e18b8e5
Victor udenna <victorudenna@gmail.com>
2024-02-26 17:55:08 +0100

    edited chat function

--- src/Components/Organism/Chatlist/Chatlist.tsx
+++ src/Components/Organism/Chatlist/Chatlist.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Input from '../../atoms/Input/Input'
 import Chatliststyle from './ChatlistStyle'
-import Image from '../../atoms/Image/Image'
 import Text from '../../atoms/Text/Text'
-import Chatlistdata from '../../../assets/data/Chatlistdata.json'
 import Gravatar from '../../atoms/Gravatar/Gravatar'
 import { HiMiniUserGroup } from 'react-icons/hi2'
 import { IoFilter } from 'react-icons/io5'
 import { useDispatch, useSelector } from 'react-redux'
 import { TypedDispatch, RootStore } from '../../../Config/configstore'
 import { saveChat } from '../../../redux/actions/ChatAction'
+import { fetchEmployeeList } from '../../../redux/actions/EmployeeAction'
 
 const Chatlist = () => {
   const [selectChat, setSelectedchat] = useState<string>('')
@@ -28,17 +27,34 @@ const Chatlist = () => {
     await dispatch(saveChat({ id: id }))
   }
 
-  const formatMessagelength = (message: string) => {
-    if (message.length > 35) {
-      const formatedText = message.slice(0, 35) + ' ...'
-      return formatedText
-    } else {
-      return message
-    }
-  }
+  // const formatMessagelength = (message: string) => {
+  //   if (message.length > 35) {
+  //     const formatedText = message.slice(0, 35) + ' ...'
+  //     return formatedText
+  //   } else {
+  //     return message
+  //   }
+  // }
 
-  const state = useSelector((state: RootStore) => state)
-  const chatId = state.chatReducer.result.data.id
+  const chatId = useSelector(
+    (state: RootStore) => state.chatReducer.result.data.id
+  )
+  const userId = useSelector(
+    (state: RootStore) => state.saveAuthReducer.result.data.uid
+  )
+
+  const { isLoading, data: employeeData } = useSelector(
+    (state: RootStore) => state.employeeReducer
+  )
+
+  useEffect(() => {
+    const getChatlist = async () => {
+      await dispatch(fetchEmployeeList(userId))
+    }
+    getChatlist()
+  }, [dispatch, userId])
+
+  console.log(employeeData, isLoading)
 
   return (
     <Chatliststyle>
@@ -57,21 +73,30 @@ const Chatlist = () => {
             <IoFilter color="228B22" />
           </div>
         </div>
-        <div className="chat_item_container">
-          {Chatlistdata.map((item: any, i) => {
-            return (
-              <div
-                key={i}
-                onClick={() => {
-                  handleChatselect(item.user_id)
-                }}
-                className={`chat_item ${
-                  selectChat == item.user_id || chatId == item.user_id
-                    ? 'active'
-                    : ''
-                }`}
-              >
-                {item.image == null ? (
+
+        {employeeData.length > 0 ? (
+          <>
+            {' '}
+            <div className="chat_item_container">
+              {employeeData.map((item: any) => {
+                return (
+                  <div
+                    key={item.id}
+                    onClick={() => {
+                      handleChatselect(item.id)
+                    }}
+                    className={`chat_item ${
+                      selectChat == item.id || chatId == item.id ? 'active' : ''
+                    }`}
+                  >
+                    <Gravatar
+                      className="chat_img"
+                      firstname={item.first_name}
+                      lastname={item.last_name}
+                      background="random"
+                      size={0.33}
+                    />
+                    {/* {item.image == null ? (
                   <Gravatar
                     className="chat_img"
                     firstname={item.first_name}
@@ -85,50 +110,67 @@ const Chatlist = () => {
                     image={item.image}
                     alt="user_img"
                   />
-                )}
-                <div
-                  className={
-                    item.active ? 'active_status_icon' : 'inactive_status_icon'
-                  }
-                ></div>
-                <div>
-                  <Text
-                    classname="message_name"
-                    value={item.first_name + ' ' + item.last_name}
-                  />
-                  <Text
-                    classname="last_text"
-                    value={
-                      item.messages.length > 0
-                        ? formatMessagelength(
-                            item.messages[item.messages.length - 1].content
-                          )
-                        : item.first_name +
+                )} */}
+                    <div
+                      className={
+                        item.is_active
+                          ? 'active_status_icon'
+                          : 'inactive_status_icon'
+                      }
+                    ></div>
+                    <div>
+                      <Text
+                        classname="message_name"
+                        value={item.first_name + ' ' + item.last_name}
+                      />
+                      <Text
+                        classname="last_text"
+                        value={
+                          // item.messages.length > 0
+                          //   ? formatMessagelength(
+                          //       item.messages[item.messages.length - 1].content
+                          //     )
+                          //   :
+                          item.first_name +
                           ' ' +
                           item.last_name +
                           ' ' +
                           'joined this work space'
-                    }
-                  />
-                </div>
-                {item.messages.length > 0 ? (
-                  <div className="message_count">{item.messages.length}</div>
-                ) : (
-                  ''
-                )}
-                {item.messages.length > 0 ? (
-                  <div className="message_time">
-                    {formatTime(
-                      item.messages[item.messages.length - 1].timestamp
+                        }
+                      />
+                    </div>
+                    {item.message && item.messages.length > 0 ? (
+                      <div className="message_count">
+                        {item.messages.length}
+                      </div>
+                    ) : (
+                      ''
+                    )}
+                    {item.message && item.messages.length > 0 ? (
+                      <div className="message_time">
+                        {formatTime(
+                          item.messages[item.messages.length - 1].timestamp
+                        )}
+                      </div>
+                    ) : (
+                      ''
                     )}
                   </div>
-                ) : (
-                  ''
-                )}
-              </div>
-            )
-          })}
-        </div>
+                )
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className='no__chatlist__container'>
+              <Text classname="no_chatlist" value={'No chat found'} />
+              <Text
+                classname="no_chatlist_desc"
+                value={`Seems you have not added an employee to this system, If you wish to comminacate, navigate to Employee to add an employee to this system`}
+              />
+            </div>
+          </>
+        )}
       </div>
     </Chatliststyle>
   )
--- src/Components/Organism/Chatlist/ChatlistStyle.ts
+++ src/Components/Organism/Chatlist/ChatlistStyle.ts
@@ -1,23 +1,21 @@
 import styled from 'styled-components'
 
 const Chatliststyle = styled.div`
-
-.chat_list{
+  .chat_list {
     height: 85vh;
     overflow-x: scroll;
     position: relative;
     -ms-overflow-style: none;
     scrollbar-width: none;
     overflow: -moz-scrollbars-none;
-
-}
-
-.chat_list::-webkit-scrollbar{
-    display: none;
-    width: 0 !important
   }
 
-  .chat__input_container{
+  .chat_list::-webkit-scrollbar {
+    display: none;
+    width: 0 !important;
+  }
+
+  .chat__input_container {
     position: sticky;
     top: 0px;
     z-index: 1;
@@ -28,100 +26,100 @@ const Chatliststyle = styled.div`
     padding: 7px 0px;
   }
 
-.search__input{
-width: 70%;
-border-radius: 4px;
-height: 34px;
-border: 1.4px solid #dfe6df;
-font-size: 12px;
-padding: 8px 12px;   
-color: grey; 
-margin: 2px 2px 0px 2px;
-}
+  .search__input {
+    width: 70%;
+    border-radius: 4px;
+    height: 34px;
+    border: 1.4px solid #dfe6df;
+    font-size: 12px;
+    padding: 8px 12px;
+    color: grey;
+    margin: 2px 2px 0px 2px;
+  }
 
-input[type="search"]:focus{
-    outline: 1.4px solid #afe1af ;
-}
+  input[type='search']:focus {
+    outline: 1.4px solid #afe1af;
+  }
 
-input[type="search"]::-webkit-search-decoration,
-input[type="search"]::-webkit-search-cancel-button,
-input[type="search"]::-webkit-search-results-button,
-input[type="search"]::-webkit-search-results-decoration {
-  display: none;
-}
+  input[type='search']::-webkit-search-decoration,
+  input[type='search']::-webkit-search-cancel-button,
+  input[type='search']::-webkit-search-results-button,
+  input[type='search']::-webkit-search-results-decoration {
+    display: none;
+  }
 
-.create__space_btn{
- background-color: #fff;
- width: 15%;
- height: 34px;
- display: flex;
- justify-content: center;
- align-items: center;
- border-radius: 4px;
- border: 1px solid #afe1af;
- cursor: pointer
-}
+  .create__space_btn {
+    background-color: #fff;
+    width: 15%;
+    height: 34px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    border: 1px solid #afe1af;
+    cursor: pointer;
+  }
 
-.filter_btn{
-  background-color: #fff;
-  width: 15%;
-  height: 34px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 4px;
-  border: 1px solid #afe1af ;
-  cursor: pointer;
- }
+  .filter_btn {
+    background-color: #fff;
+    width: 15%;
+    height: 34px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    border: 1px solid #afe1af;
+    cursor: pointer;
+  }
 
- .filter_btn:hover, .create__space_btn:hover{
-  background-color: honeydew;
- }
+  .filter_btn:hover,
+  .create__space_btn:hover {
+    background-color: honeydew;
+  }
 
-.chat_item{
-display: flex;
-align-items: center;
-padding: 12px;
-gap: 12px;
-margin: 12px 2px;
-border-bottom: 1px solid #dfe6df;
-position: relative;
-cursor: context-menu;
-.message_name{
-    font-size 10px;
-    padding-bottom: 4px;
-     }
-   
-     .last_text{
-       font-size: 11px;
-     }
-}
+  .chat_item {
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    gap: 12px;
+    margin: 12px 2px;
+    border-bottom: 1px solid #dfe6df;
+    position: relative;
+    cursor: context-menu;
+    .message_name {
+      font-size: 10px;
+      padding-bottom: 4px;
+    }
 
-.chat_item.active{
-   width: 99%; 
-  border-left: 2px solid #228B22;
-  .message_name{
-   font-weight: 600;
-     } 
-}
+    .last_text {
+      font-size: 11px;
+    }
+  }
 
-.chat_img{
-   width: 50px;
-   height: 50px; 
-   border-radius: 50%;
-}
-.active_status_icon{
+  .chat_item.active {
+    width: 99%;
+    border-left: 2px solid #228b22;
+    .message_name {
+      font-weight: 600;
+    }
+  }
+
+  .chat_img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+  .active_status_icon {
     width: 13px;
     height: 13px;
-    background-color: #228B22;
+    background-color: #228b22;
     border-radius: 50%;
     position: absolute;
     left: 45px;
     bottom: 10px;
+  }
 
-}
-
-.inactive_status_icon{
+  .inactive_status_icon {
     width: 13px;
     height: 13px;
     background-color: orange;
@@ -129,32 +127,62 @@ cursor: context-menu;
     position: absolute;
     left: 45px;
     bottom: 10px;
-}
+  }
 
-.message_count{
-background-color: #228B22;
-color: white;
-width: 15px;
-height: 15px;
-border-radius: 50%;
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: 10px;
-padding: 2px;
-position: absolute;
-right: 4px;
-top: 20px;
-}
+  .message_count {
+    background-color: #228b22;
+    color: white;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 10px;
+    padding: 2px;
+    position: absolute;
+    right: 4px;
+    top: 20px;
+  }
 
-.message_time{
-font-size: 11px;  
-color: #228B22;
-position: absolute;
-right: 0px;
-top: 40px;
-}
+  .message_time {
+    font-size: 11px;
+    color: #228b22;
+    position: absolute;
+    right: 0px;
+    top: 40px;
+  }
 
+  .no__chatlist__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .no_chatlist {
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 140%;
+    letter-spacing: -0.36px;
+    color: #051226;
+    font-size: 24px;
+    padding: 28px 0px;
+  }
+
+  .no_chatlist_desc {
+    font-size: 14px !important;
+    font-style: normal;
+    font-weight: 400;
+    text-align: center;
+    line-height: 160%;
+    letter-spacing: -0.36px;
+    color: #051226;
+    font-size: 24px;
+    padding: 28px 0px;
+  }
 `
 
 export default Chatliststyle
